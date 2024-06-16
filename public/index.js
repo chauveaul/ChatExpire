5bd04931b95fc19a7f32cb88181aef172b708317
MeZeOfficial <lucaschauveau007@gmail.com>
2024-06-15 20:39:29 -0400

    squash! Initial commit
    	new file:   .DS_Store
    	new file:   .firebaserc
    	new file:   .gitignore
    	new file:   firebase.json
    	new file:   firestore.indexes.json
    	new file:   firestore.rules
    	new file:   public/404.html
    	new file:   public/index.html
    	new file:   public/index.js

---
+++ public/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxJlHA4Y5uC5_aJbzqIFlhG1rbikc2fPM",
+  authDomain: "chatexpire.firebaseapp.com",
+  projectId: "chatexpire",
+  storageBucket: "chatexpire.appspot.com",
+  messagingSenderId: "955509183528",
+  appId: "1:955509183528:web:f1ae208a9d4667c6de911d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
